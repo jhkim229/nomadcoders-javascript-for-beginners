35925aa10965af03a484578b7868bbb2da56516a
Jinhui Kim <202204487@hufs.ac.kr>
2024-07-21 00:43:06 +0900

    #4.2 Events

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-    const userName = loginInput.value;
-    console.log(userName);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick)
+loginForm.addEventListener("submit", onLoginSubmit);
