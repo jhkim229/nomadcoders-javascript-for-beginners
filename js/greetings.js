b67b6ae28af6270d11be5103392e6ca9f0b54d55
Jinhui Kim <202204487@hufs.ac.kr>
2024-07-21 02:06:58 +0900

    #5.0 Intervals

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,6 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
-const geeting = document.querySelector('#greeting');
+const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
 const USERNAME_KEY = 'username';
@@ -17,13 +17,12 @@ function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-  // show the form
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener('submit', onLoginSubmit);
 } else {
-  // show the greetings
   paintGreetings(savedUsername);
 }
