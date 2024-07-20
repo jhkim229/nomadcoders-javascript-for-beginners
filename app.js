422227992c0e26c327e7b662466f89f2321c4c13
Jinhui Kim <202204487@hufs.ac.kr>
2024-07-21 01:44:48 +0900

    #4.6 Loading Username

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ const loginInput = document.querySelector('#login-form input');
 const geeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener('submit', onLoginSubmit);
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+  // show the greetings
+  paintGreetings(savedUsername);
+}
